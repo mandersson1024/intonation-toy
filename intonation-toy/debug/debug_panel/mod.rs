464626aa8541a2c14d8aa8a567c42de70ab5814e
mandersson1024 <mixand@gmail.com>
2025-08-04 15:30:04 +0200

    Remove dead code

--- intonation-toy/debug/debug_panel/mod.rs
+++ intonation-toy/debug/debug_panel/mod.rs
@@ -5,9 +5,7 @@
 // Debug controls use the new presenter action collection system
 // All debug functionality is only available in debug builds
 
-pub(crate) mod overlay;
 pub(crate) mod debug_panel;
 pub(crate) mod data_types;
 
-pub(crate) use overlay::EguiDebugControls;
 pub(crate) use debug_panel::DebugPanel;
