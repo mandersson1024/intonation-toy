a2ff9c32617738897d2bf7f1a84c1eab4210b920
mandersson1024 <mixand@gmail.com>
2025-08-23 10:57:44 +0200

    simplify web utils

--- intonation-toy/src/web/utils.rs
+++ intonation-toy/src/web/utils.rs
@@ -1,16 +1,4 @@
-//! Web utility functions for color conversion and other common operations
 
-/// Convert RGB values to CSS rgba() string
-pub fn rgba_to_css(rgb: [f32; 3], alpha: f32) -> String {
-    format!("rgba({}, {}, {}, {})", 
-        (rgb[0] * 255.0) as u8, 
-        (rgb[1] * 255.0) as u8, 
-        (rgb[2] * 255.0) as u8,
-        alpha
-    )
-}
-
-/// Convert RGB values to CSS rgb() string
 pub fn rgb_to_css(rgb: [f32; 3]) -> String {
     format!("rgb({}, {}, {})", 
         (rgb[0] * 255.0) as u8, 
