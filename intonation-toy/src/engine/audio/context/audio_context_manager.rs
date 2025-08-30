688ab3558a9d4b820e55e636f05f77bd19c1194b
mandersson1024 <mixand@gmail.com>
2025-08-30 21:00:22 +0200

    delete dead code

--- intonation-toy/src/engine/audio/context/audio_context_manager.rs
+++ intonation-toy/src/engine/audio/context/audio_context_manager.rs
@@ -1,8 +1,7 @@
-use web_sys::{AudioContext, AudioContextOptions};
+use web_sys::AudioContext;
 use crate::common::dev_log;
 use super::super::AudioError;
 use super::AudioContextState;
-use crate::app_config::STANDARD_SAMPLE_RATE;
 
 pub struct AudioContextManager {
     context: Option<AudioContext>,
@@ -22,24 +21,6 @@ impl Default for AudioContextManager {
 impl AudioContextManager {
     pub fn state(&self) -> &AudioContextState {
         &self.state
-    }
-    
-    pub fn initialize(&mut self) -> Result<(), AudioError> {
-        self.state = AudioContextState::Initializing;
-        let options = AudioContextOptions::new();
-        options.set_sample_rate(STANDARD_SAMPLE_RATE as f32);
-        
-        let context = AudioContext::new_with_context_options(&options)
-            .map_err(|e| {
-                dev_log!("✗ Failed to create AudioContext: {:?}", e);
-                self.state = AudioContextState::Closed;
-                AudioError::StreamInitFailed(format!("Failed to create AudioContext: {:?}", e))
-            })?;
-            
-        dev_log!("✓ AudioContext created");
-        self.context = Some(context);
-        self.state = AudioContextState::Running;
-        Ok(())
     }
     
     pub fn close(&mut self) -> Result<(), AudioError> {
